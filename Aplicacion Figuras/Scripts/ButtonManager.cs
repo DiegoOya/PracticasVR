a225a4d6b9681a22acce2c7cdb97e7a1c043d39f
Diego Oya <diegooya5@gmail.com>
2017-07-18 12:42:31 +0200

    Cambiado el ButtonManager

--- Aplicacion Figuras/Scripts/ButtonManager.cs
+++ Aplicacion Figuras/Scripts/ButtonManager.cs
@@ -25,98 +25,198 @@ public class ButtonManager : MonoBehaviour
         }
     }
 
-    //Si se pulsa el boton esfera se llama a la funcion que instancia una esfera en el mando correspondiente
-    public void Esfera_clicked()
-    {
-        if (mandoHTC.GetComponent<PlayerHTC>().isLocalPlayer)
-        {
-            if (mandoIzq.GetComponent<SteamVR_TrackedController>().triggerPressed)
-            {
-                spawner.GetComponent<SpawnerController>().EsferaClicked(mandoIzq);
-            }
-            else
-            {
-				spawner.GetComponent<SpawnerController>().EsferaClicked(mandoDer);
-            }
-        }else if(mandoRaton.GetComponent<PlayerControllerRaton>().isLocalPlayer)
-        {
-            spawner.GetComponent<SpawnerController>().EsferaClicked(mandoRaton);
-        }
-    }
+	//Si se pulsa el boton esfera se llama a la funcion que instancia una esfera en el mando correspondiente
+	public void Esfera_clicked()
+	{
+		if (mandoHTC != null)
+		{
+			if (mandoHTC.GetComponent<PlayerHTC>().isLocalPlayer)
+			{
+				if (mandoIzq.GetComponent<SteamVR_TrackedController>().triggerPressed)
+				{
+					spawner.GetComponent<SpawnerController>().EsferaClicked(mandoIzq);
+				}
+				else
+				{
+					spawner.GetComponent<SpawnerController>().EsferaClicked(mandoDer);
+				}
+			}
+			else
+			{
+				if (mandoRaton != null)
+				{
+					if (mandoRaton.GetComponent<PlayerControllerRaton>().isLocalPlayer)
+					{
+						spawner.GetComponent<SpawnerController>().EsferaClicked(mandoRaton);
+					}
+				}
+			}
+		}
+		else
+		{
+			if (mandoRaton != null)
+			{
+				if (mandoRaton.GetComponent<PlayerControllerRaton>().isLocalPlayer)
+				{
+					spawner.GetComponent<SpawnerController>().EsferaClicked(mandoRaton);
+				}
+			}
+		}
+	}
 
     //Si se pulsa el boton cubo se llama a la funcion que instancia un cubo en el mando correspondiente
     public void Cubo_clicked()
     {
-        if (mandoHTC.GetComponent<PlayerHTC>().isLocalPlayer)
-        {
-            if (mandoIzq.GetComponent<SteamVR_TrackedController>().triggerPressed)
-            {
-				spawner.GetComponent<SpawnerController>().CuboClicked(mandoIzq);
-            }
-            else
-            {
-				spawner.GetComponent<SpawnerController>().CuboClicked(mandoDer);
-            }
-        }else if(mandoRaton.GetComponent<PlayerControllerRaton>().isLocalPlayer)
-        {
-            spawner.GetComponent<SpawnerController>().CuboClicked(mandoRaton);
-        }
-    }
+		if (mandoHTC != null)
+		{
+			if (mandoHTC.GetComponent<PlayerHTC>().isLocalPlayer)
+			{
+				if (mandoIzq.GetComponent<SteamVR_TrackedController>().triggerPressed)
+				{
+					spawner.GetComponent<SpawnerController>().CuboClicked(mandoIzq);
+				}
+				else
+				{
+					spawner.GetComponent<SpawnerController>().CuboClicked(mandoDer);
+				}
+			}
+			else
+			{
+				if (mandoRaton != null)
+				{
+					if (mandoRaton.GetComponent<PlayerControllerRaton>().isLocalPlayer)
+					{
+						spawner.GetComponent<SpawnerController>().CuboClicked(mandoRaton);
+					}
+				}
+			}
+		}
+		else
+		{
+			if (mandoRaton != null)
+			{
+				if (mandoRaton.GetComponent<PlayerControllerRaton>().isLocalPlayer)
+				{
+					spawner.GetComponent<SpawnerController>().CuboClicked(mandoRaton);
+				}
+			}
+		}
+	}
 
     //Si se pulsa el boton capsula se llama a la funcion que instancia una capsula en el mando correspondiente
     public void Capsula_clicked()
     {
-        if (mandoHTC.GetComponent<PlayerHTC>().isLocalPlayer)
-        {
-            if (mandoIzq.GetComponent<SteamVR_TrackedController>().triggerPressed)
-            {
-				spawner.GetComponent<SpawnerController>().CapsulaClicked(mandoIzq);
-            }
-            else
-            {
-				spawner.GetComponent<SpawnerController>().CapsulaClicked(mandoDer);
-            }
-        }else if(mandoRaton.GetComponent<PlayerControllerRaton>().isLocalPlayer)
-        {
-            spawner.GetComponent<SpawnerController>().CapsulaClicked(mandoRaton);
-        }
-    }
+		if (mandoHTC != null)
+		{
+			if (mandoHTC.GetComponent<PlayerHTC>().isLocalPlayer)
+			{
+				if (mandoIzq.GetComponent<SteamVR_TrackedController>().triggerPressed)
+				{
+					spawner.GetComponent<SpawnerController>().CapsulaClicked(mandoIzq);
+				}
+				else
+				{
+					spawner.GetComponent<SpawnerController>().CapsulaClicked(mandoDer);
+				}
+			}
+			else
+			{
+				if (mandoRaton != null)
+				{
+					if (mandoRaton.GetComponent<PlayerControllerRaton>().isLocalPlayer)
+					{
+						spawner.GetComponent<SpawnerController>().CapsulaClicked(mandoRaton);
+					}
+				}
+			}
+		}
+		else
+		{
+			if (mandoRaton != null)
+			{
+				if (mandoRaton.GetComponent<PlayerControllerRaton>().isLocalPlayer)
+				{
+					spawner.GetComponent<SpawnerController>().CapsulaClicked(mandoRaton);
+				}
+			}
+		}
+	}
 
     // Si se pulsa el boton mover se asocia el objeto al mando correspondiente
     public void MoverClickado()
     {
-        if (mandoHTC.GetComponent<PlayerHTC>().isLocalPlayer)
-        {
-            if (mandoIzq.GetComponent<SteamVR_TrackedController>().triggerPressed)
-            {
-                mandoIzq.GetComponent<SteamVR_LaserPointer>().MoverClickado();
-            }
-            else
-            {
-                mandoDer.GetComponent<SteamVR_LaserPointer>().MoverClickado();
-            }
-        }else if(mandoRaton.GetComponent<PlayerControllerRaton>().isLocalPlayer)
-        {
-            mandoRaton.GetComponent<PlayerControllerRaton>().MoverClickado();
-        }
-    }
+       if (mandoHTC != null)
+		{
+			if (mandoHTC.GetComponent<PlayerHTC>().isLocalPlayer)
+			{
+				if (mandoIzq.GetComponent<SteamVR_TrackedController>().triggerPressed)
+				{
+					mandoIzq.GetComponent<SteamVR_LaserPointer>().MoverClickado();
+				}
+				else
+				{
+					mandoDer.GetComponent<SteamVR_LaserPointer>().MoverClickado();
+				}
+			}
+			else
+			{
+				if (mandoRaton != null)
+				{
+					if (mandoRaton.GetComponent<PlayerControllerRaton>().isLocalPlayer)
+					{
+						mandoRaton.GetComponent<PlayerControllerRaton>().MoverClickado();
+					}
+				}
+			}
+		}
+		else
+		{
+			if (mandoRaton != null)
+			{
+				if (mandoRaton.GetComponent<PlayerControllerRaton>().isLocalPlayer)
+				{
+					mandoRaton.GetComponent<PlayerControllerRaton>().MoverClickado();
+				}
+			}
+		}
+	}
 
     //Si se pulsa el boton color se llama a la funcion que cambia el color de las figuras en el mando correspondiente
     public void ColorClickado()
     {
-        if (mandoHTC.GetComponent<PlayerHTC>().isLocalPlayer)
-        {
-            if (mandoIzq.GetComponent<SteamVR_TrackedController>().triggerPressed)
-            {
-                mandoIzq.GetComponent<SteamVR_LaserPointer>().ColorClickado();
-            }
-            else
-            {
-                mandoDer.GetComponent<SteamVR_LaserPointer>().ColorClickado();
-            }
-        }else if(mandoRaton.GetComponent<PlayerControllerRaton>().isLocalPlayer)
-        {
-            mandoRaton.GetComponent<PlayerControllerRaton>().ColorClickado();
-        }
-    }
+		if (mandoHTC != null)
+		{
+			if (mandoHTC.GetComponent<PlayerHTC>().isLocalPlayer)
+			{
+				if (mandoIzq.GetComponent<SteamVR_TrackedController>().triggerPressed)
+				{
+					mandoIzq.GetComponent<SteamVR_LaserPointer>().ColorClickado();
+				}
+				else
+				{
+					mandoDer.GetComponent<SteamVR_LaserPointer>().ColorClickado();
+				}
+			}
+			else
+			{
+				if (mandoRaton != null)
+				{
+					if (mandoRaton.GetComponent<PlayerControllerRaton>().isLocalPlayer)
+					{
+						mandoRaton.GetComponent<PlayerControllerRaton>().ColorClickado();
+					}
+				}
+			}
+		}
+		else
+		{
+			if (mandoRaton != null)
+			{
+				if (mandoRaton.GetComponent<PlayerControllerRaton>().isLocalPlayer)
+				{
+					mandoRaton.GetComponent<PlayerControllerRaton>().ColorClickado();
+				}
+			}
+		}
+	}
 }
